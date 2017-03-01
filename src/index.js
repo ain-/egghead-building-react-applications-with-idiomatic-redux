ff81e9f0b71e8ab6ca27827f91522b124355fd65
Ain <ain.salula@gmail.com>
2017-03-01 07:47:48 +0200

    use throttle to limit saves to localStorage

--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ import ReactDOM from 'react-dom';
 import TodoApp from './TodoApp';
 import todoApp from './reducers/index';
 import { loadState, saveState } from './localStorage';
+import throttle from 'lodash/throttle';
 
 const persistedState = loadState();
 
 const store = createStore(todoApp, persistedState);
-store.subscribe(() => {
+store.subscribe(throttle(() => {
   saveState({
     todos: store.getState().todos
   });
-});
+}, 1000));
 
 ReactDOM.render(
   <Provider store={store}>
