2128a708581a4e8af2ea43ac1357e86572ce53ee
Ain <ain.salula@gmail.com>
2017-03-01 02:32:18 +0200

    passing persisted state to the store on creation

--- src/index.js
+++ src/index.js
@@ -5,9 +5,19 @@ import ReactDOM from 'react-dom';
 import TodoApp from './TodoApp';
 import todoApp from './reducers/index';
 
+const persistedState = {
+  todos: [{
+    id: '0',
+    text: 'Welcome back!',
+    completed: false
+  }]
+};
+
+const store = createStore(todoApp, persistedState);
+console.log(store.getState());
 
 ReactDOM.render(
-  <Provider store={createStore(todoApp)}>
+  <Provider store={store}>
     <TodoApp />
   </Provider>,
   document.getElementById('root')
