da8d099b290090115c0ca528f852095162a66b3e
Ain <ain.salula@gmail.com>
2017-03-02 18:51:21 +0200

    wrap dispatch() to log actions

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,10 +3,31 @@ import todoApp from './reducers';
 import { createStore } from 'redux';
 import { loadState, saveState } from './localStorage';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+    return rawDispatch;
+  }
+
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  }
+}
+
 const configureStore = () => {
   const persistedState = loadState();
-
   const store = createStore(todoApp, persistedState);
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
+
   store.subscribe(throttle(() => {
     saveState({
       todos: store.getState().todos
