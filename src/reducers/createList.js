b308016513c6c5dfa31a25d5a039cefe8f478d61
Ain <ain.salula@gmail.com>
2017-03-04 01:14:08 +0200

    refactoring reducers

---
+++ src/reducers/createList.js
@@ -0,0 +1,15 @@
+const createList = (filter) => {
+  return (state = [], action) => {
+    if (action.filter !== filter) {
+      return state;
+    }
+    switch (action.type) {
+      case 'RECEIVE_TODOS':
+        return action.response.map(todo => todo.id);
+      default:
+        return state;
+    }
+  };
+};
+
+export default createList;
