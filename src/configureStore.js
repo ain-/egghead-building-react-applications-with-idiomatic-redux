ef8132783d8972d8dc6d96d0d5cb1e089df39fcb
Ain <ain.salula@gmail.com>
2017-03-03 04:51:59 +0200

    simplify functions

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,33 +1,26 @@
 import todoApp from './reducers';
 import { createStore } from 'redux';
 
-const logger = (store) => {
-  return (next) => {
-    if (!console.group) {
-      return next;
-    }
-
-    return (action) => {
-      console.group(action.type);
-      console.log('%c prev state', 'color: gray', store.getState());
-      console.log('%c action', 'color: blue', action);
-      const returnValue = next(action);
-      console.log('%c next state', 'color: green', store.getState());
-      console.groupEnd(action.type);
-      return returnValue;
-    };
+const logger = (store) => (next) => {
+  if (!console.group) {
+    return next;
+  }
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = next(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
   };
 };
 
-const promise = (store) => {
-  return (next) => {
-    return (action) => {
-      if (typeof action.then === 'function') {
-        return action.then(next);
-      }
-      return next(action);
-    };
-  };
+const promise = (store) => (next) => (action) => {
+  if (typeof action.then === 'function') {
+    return action.then(next);
+  }
+  return next(action);
 };
 
 const wrapDispatchWithMiddlewares = (store, middlewares) => {
