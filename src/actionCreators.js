c3834147cfa25232620f077d3818359c9c3c83bc
Ain <ain.salula@gmail.com>
2017-03-05 02:22:53 +0200

    add todo fake api

--- src/actionCreators.js
+++ src/actionCreators.js
@@ -1,4 +1,3 @@
-import { v4 } from 'node-uuid';
 import * as api from './api';
 import { getIsFetching } from './reducers';
 
@@ -34,8 +33,10 @@ export const toggleTodo = (id) => ({
   id
 });
 
-export const addTodo = (text) => ({
-  type: 'ADD_TODO',
-  id: v4(),
-  text: text
-});
+export const addTodo = (text) => (dispatch) =>
+  api.addTodo(text).then(response => {
+    dispatch({
+      type: 'ADD_TODO_SUCCESS',
+      response
+    })
+  })
